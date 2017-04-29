ca040e2887f335b6846eebb4925699813aa0b991
kkhalifah <karen.khalifah@gmail.com>
2017-04-29 17:12:25 -0400

    fitst commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function vintagemotorcycles() {
+
+      $.get('http://www.reddit.com/r/vintagemotorcycles.json').done(function(response) {
+
+      console.log(response.data.children[0].data);
+
+      for (var i = 0; i < response.data.children.length; i++) {
+          var title = response.data.children[i].data.title;
+          var author = response.data.children[i].data.author;
+          var thumbnail = response.data.children[i].data.thumbnail;
+
+            $('body').append('<div class=container><p id= myAuthor>' + author + '</p><p id=myTitle>' + title + '</p><img src=' + thumbnail + '></div>');
+            //console.log(i);
+      }
+    });
+
+  }
+      vintagemotorcycles();
+//end of doc ready function
+});
